9d9a746ea78cdcbe9f677a801081c242b41f9412
Dimitrios Gretsistas <dimitriosgretsistas@gmail.com>
2023-09-19 15:38:43 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import StudentList from './components/StudentList';
+import MovieList from './components/MovieList';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+           {/* <List /> */}
+      {/* <StudentList /> */}
+      {/* <ProjectList /> */}
+ 
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
